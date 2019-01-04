db4df6c4dd34ad110d53732fcd5efb0cdfc59fe4
jiro4989 <jiroron666@gmail.com>
2019-01-04 21:46:37 +0900

    :sparkles:修正

--- src/image.nim
+++ src/image.nim
@@ -6,7 +6,7 @@
 # Pythonで画像生成 http://d.hatena.ne.jp/c-yan2/20100317/1268825146
 
 import crc32
-import strutils, streams, algorithm
+import strutils, streams, algorithm, sequtils
 
 type
   RGBA = object
@@ -27,6 +27,9 @@ type
 proc toHexStr(n: int, digit: int): string =
   n.toHex(digit).parseHexStr
 
+proc chunk(t: string, d: string): string =
+  d.len.toHexStr(8) & t & d
+
 var
   imageWidth = 701
   imageHeight = 191
@@ -37,59 +40,29 @@ defer: s.close
 for data in ['\x89', 'P', 'N', 'G', '\x0D', '\x0A', '\x1A', '\x0A']:
   s.write data
 
-# IHDRチャンク: 25byte: イメージヘッダ https://www.setsuki.com/hsp/ext/chunk/IHDR.htm
-## (4)length
-s.write 13.toHexStr 8
-
-## (4)chunk type
-s.write "IHDR"
-
-## chunk data
-
-# (4)画像の横幅
-s.write imageWidth.toHexStr 8
-
-# (4)画像の縦幅
-s.write imageHeight.toHexStr 8
-
-# (1)ビット深度
-# TODO: とりあえず決め打ち
-#s.write 0x08'u8
-s.write 8.toHexStr 2
-
-# (1)カラータイプ
-# TODO: とりあえず決め打ち
-s.write 6.toHexStr 2
-
-# (1)圧縮手法
-# TODO: とりあえず決め打ち
-s.write 0.toHexStr 2
-
-# (1)フィルター手法
-s.write 0.toHexStr 2
-
-# (1)インターレース手法
-# なし = 0, あり = 1
-s.write 0.toHexStr 2
+var data = [
+  [imageWidth, 8],  # (4)画像の横幅
+  [imageHeight, 8], # (4)画像の縦幅
+  [8, 2],           # (1)ビット深度
+  [6, 2],           # (1)カラータイプ
+  [0, 2],           # (1)圧縮手法
+  [0, 2],           # (1)フィルター手法
+  [0, 2]            # (1)インターレース手法
+]
 
 # (4)CRC (cyclic redundancy check)
 # chunk type と chunk dataで計算する
 # https://qiita.com/mikecat_mixc/items/e5d236e3a3803ef7d3c5
-let w = imageWidth.toHexStr 8
-let h = imageHeight.toHexStr 8
-s.write crc32("IHDR" & w & h & $0x06'u8 & $0x08'u8 & $0x00'u8 & $0x00'u8 & $0x00'u8)
+
+var dataDiv = data.mapIt(it[0].toHexStr it[1]).join("")
+s.write chunk("IHDR", dataDiv)
+s.write crc32("IHDR" & dataDiv)
+echo chunk("IHDR", dataDiv).len
+echo len($(crc32("IHDR" & dataDiv)))
 
 # IDATチャンク: 可変長: イメージデータ
 # TODO
 
 # IENDチャンク: 12byte: イメージ終端
-## (4)length
-s.write '\x00' # 固定
-s.write '\x00' # 固定
-s.write '\x00' # 固定
-s.write '\x00' # 固定
-
-## (4)chunk type
-s.write "IEND"
-## (4)crc
-#s.write crc32("IEND")
+s.write chunk("IEND", "")
+s.write crc32("IEND" & "")
